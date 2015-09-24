f28ff9e1ad929bdaa085381d595bc44a1c7e268a
liujian <love-liu.99@163.com>
2015-09-24 08:55:41 +0800

    添加substring_filters文件。

---
+++ SimpleTaskSystem.WebSpaAngular/App/Main/filters/substring_filters.js
@@ -0,0 +1,9 @@
+﻿angular.module('app')
+    .filter('subString', function () {
+        return function (input,num) {
+            if (input.length > num) {
+                return input.substring(0, num) + "...";
+            }
+            return input;
+        };
+    });
